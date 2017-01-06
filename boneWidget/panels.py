d8c76957c12ae6d6fb960c4ddb3c4dd3908ad103
ChristopheSeux <seuxchristophe@hotmail.fr>
2017-01-06 23:23:25 +0100

    Widget list auto compact when superior to 6

--- boneWidget/panels.py
+++ boneWidget/panels.py
@@ -17,18 +17,17 @@ class bw_posemode_panel(bpy.types.Panel):
     for key in sorted(items):
         itemsSort.append((key,key,""))
 
-    bpy.types.Scene.widget_list = bpy.props.EnumProperty(name="Shape",items=itemsSort,description="Shape" )   
-    bpy.types.Scene.contract_list = bpy.props.BoolProperty(name = "Contract_list", description = "Contract_list")
-    
+    bpy.types.Scene.widget_list = bpy.props.EnumProperty(name="Shape",items=itemsSort,description="Shape" )
+
     def draw(self, context):
-        layout = self.layout     
-        row = layout.row(align= True)        
-        
-        if context.scene.contract_list==False :
+        layout = self.layout
+        row = layout.row(align= True)
+
+        if len(bpy.types.Scene.widget_list[1]['items'])<6 :
             row.prop(context.scene, "widget_list", expand=True)
         else :
             row.prop(context.scene, "widget_list", expand=False,text="")
-        
+
         row = layout.row(align=True)
         row.menu("bw_specials", icon='DOWNARROW_HLT', text="")
         row.operator("bonewidget.create_widget",icon="OBJECT_DATAMODE")
@@ -37,7 +36,7 @@ class bw_posemode_panel(bpy.types.Panel):
             row.operator("bonewidget.edit_widget", icon ="OUTLINER_DATA_MESH")
         else :
             row.operator("bonewidget.return_to_armature",icon ="LOOP_BACK",text='To bone')
-        
+
 class bw_specials(Menu):
     bl_label = "Bone widget Specials"
 
@@ -47,4 +46,3 @@ class bw_specials(Menu):
         layout.operator("bonewidget.match_bone_transforms", icon = 'GROUP_BONE')
         layout.operator("bonewidget.add_widgets",icon = "ZOOMIN",text="Add Widgets")
         layout.operator("bonewidget.remove_widgets",icon = "ZOOMOUT",text="Remove Widgets")
-        layout.prop(context.scene, "contract_list")  
